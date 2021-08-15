5f5fe6b2416dc5ecf98142294fbd88b5ca6ac1b7
shubham sapkal <ssapkal101@gmail.com>
2021-08-15 12:49:44 +0530

    Create Q2.py

---
+++ Assignment 3/Q2.py
@@ -0,0 +1,28 @@
+# Name : Shubham Sapkal Roll No. : 2012118
+# Given a list slice it into 3 equal chunks and reverse each chunk .
+'''
+Original list [11, 45, 8, 23, 14, 12, 78, 45, 89]
+Chunk 1 [11, 45, 8]
+After reversing it [8, 45, 11]
+Chunk 2 [23, 14, 12]
+After reversing it [12, 14, 23]
+Chunk 3 [78, 45, 89]
+After reversing it [89, 45, 78]
+'''
+sampleList = [11, 45, 8, 23, 14, 12, 78, 45, 89]
+print("Original list ", sampleList)
+
+length = len(sampleList)
+chunkSize  = int(length/3)
+start = 0
+end = chunkSize
+for i in range(1, 4, 1):
+  indexes = slice(start, end, 1)
+  listChunk = sampleList[indexes]
+  print("Chunk ", i , listChunk)
+  print("After reversing it ", list(reversed(listChunk)))
+  start = end
+  if(i != 2):
+    end +=chunkSize
+  else:
+    end += length - chunkSize
